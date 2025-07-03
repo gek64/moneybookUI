f8c30cf22ff31d38f701901a741f9ebee3079e4e
gek64 <10346089+gek64@users.noreply.github.com>
2025-07-03 17:25:26 +0800

    update env

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ import {ENVIRONMENT} from "../app/share/definition/environment"
 
 export const environmentDev: ENVIRONMENT = {
     production: false,
-    server: "http://192.168.1.179:8000"
+    server: "http://localhost:8000"
 }

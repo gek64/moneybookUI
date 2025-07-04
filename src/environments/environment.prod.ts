2c97e8c9dae893a56a583239aacead73d4ed3213
gek64 <10346089+gek64@users.noreply.github.com>
2025-07-04 10:08:49 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import {ENVIRONMENT} from "../app/share/definition/environment"
 
 export const environmentProd: ENVIRONMENT = {
     production: true,
-    server: "http://server.internal:8000"
+    server: "http://nas.internal:8000"
 }
